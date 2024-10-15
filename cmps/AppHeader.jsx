e2eae84ff39bd7c7145f0e2af20fd33feb2dc0f4
Refael Levi <refael14levi@gmail.com>
2024-10-15 20:16:35 +0300

    header fix

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,38 +1,37 @@
-
-
 const { Link, NavLink } = ReactRouterDOM
-const { useEffect, useState } = React
-import { mailService } from "../apps/mail/services/mail.service.js";
-
+const { useState} = React
 export function AppHeader() {
+    const [isClicked, setIsClicked] = useState(false)
 
-
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        mailService.getMails().then(() => {
-            setLoading(false)
-        })
-    }, [])
-
-    return <header className="app-header">
-        <Link to="/">
-        <img src="assets/img/svg/hours.svg" className="img-logo img-home" />
-        </Link>
-        <nav>
-            <NavLink to="/"><img src="assets/img/home.png" className="img-logo " /></NavLink>
-            <NavLink to="/about"><img src="assets/img/about.png" className="img-logo " /></NavLink>
-            <NavLink to="/mail">
-                    {loading ? (
-                        <div className="loading-spinner"></div> 
-                    ) : (
-                        <img src="assets/img/email.png" className="img-logo" alt="Mail" /> // Show email icon when not loading
-                    )}
-                </NavLink>
-            <NavLink to="/note"><img src="assets/img/note.png" className="img-logo" /></NavLink>
-        </nav>
-    </header>
-    }
-
-
-{/* <NavLink to="/mail"><img src="assets/img/email.png" className="img-logo" /></NavLink> */}
+    return (
+        <header className="app-header">
+            <Link to="/">
+                <img src="assets/img/svg/hours.svg" className="img-logo img-home" alt="Logo" />
+            </Link>
+            <nav>
+                <span 
+                    onClick={() => setIsClicked(!isClicked)} 
+                    className="material-symbols-outlined menu"
+                >
+                    menu
+                </span>
+                {isClicked && (
+                    <div className="header-modal">
+                        <NavLink onClick={() => setIsClicked(false)} to="/">
+                            <img src="assets/img/home.png" className="img-logo" alt="Home" />
+                        </NavLink>
+                        <NavLink onClick={() => setIsClicked(false)} to="/about">
+                            <img src="assets/img/about.png" className="img-logo" alt="About" />
+                        </NavLink>
+                        <NavLink onClick={() => setIsClicked(false)} to="/mail">
+                            <img src="assets/img/email.png" className="img-logo" alt="Mail" />
+                        </NavLink>
+                        <NavLink onClick={() => setIsClicked(false)} to="/note">
+                            <img src="assets/img/note.png" className="img-logo" alt="Note" />
+                        </NavLink>
+                    </div>
+                )}
+            </nav>
+        </header>
+    )
+}

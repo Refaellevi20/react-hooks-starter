c093a6b5541d28aae617b08f9aa8dbd161a2d8e2
Elinor <israeli.elinor@gmail.com>
2024-10-15 14:04:50 +0300

    App header

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -4,11 +4,11 @@ export function AppHeader() {
 
     return <header className="app-header">
         <Link to="/">
-        <img src="assets/img/svg/hours.svg" className="img-logo" />
+        <img src="assets/img/svg/hours.svg" className="img-logo img-home" />
         </Link>
         <nav>
-            <NavLink to="/"><img src="assets/img/svg/home.svg" className="img-logo" /></NavLink>
-            <NavLink to="/about">About</NavLink>
+            <NavLink to="/"><img src="assets/img/home.png" className="img-logo " /></NavLink>
+            <NavLink to="/about"><img src="assets/img/about.png" className="img-logo " /></NavLink>
             <NavLink to="/mail"><img src="assets/img/email.png" className="img-logo" /></NavLink>
             <NavLink to="/note"><img src="assets/img/note.png" className="img-logo" /></NavLink>
         </nav>

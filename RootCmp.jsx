9b85533be14c0496f9d9b4c83052111de4426b6b
Refael Levi <refael14levi@gmail.com>
2024-10-17 21:33:27 +0300

    BookIndex on //

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -32,10 +32,10 @@ export function App() {
                 <main>
                     <Routes>
                         <Route path="/" element={<Home />} />
-                        <Route path="/book/:bookId" element={<BookBDetails />} />
-                        <Route path='/book/edit' element={<BookEdit />}  />
-                        <Route path='/book/edit/:bookId' element={<BookEdit />}/>
-                        <Route path="/book" element={<BookIndex />} />
+                        {/* <Route path="/book/:bookId" element={<BookBDetails />} /> */}
+                        {/* <Route path='/book/edit' element={<BookEdit />}  /> */}
+                        {/* <Route path='/book/edit/:bookId' element={<BookEdit />}/> */}
+                        {/* <Route path="/book" element={<BookIndex />} /> */}
                         <Route path="/mail/send" element={<ModalCompose />} />
                         <Route path="/mail" element={<MailIndex />} />
                         <Route path="/mail/:mailId" element={<MailDetails />} />

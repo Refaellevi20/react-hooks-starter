2bec549ae60d00835fab4c7a23ce67c3a4a685c2
Refael Levi <refael14levi@gmail.com>
2024-10-18 01:52:37 +0300

    cleaning

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,20 +1,30 @@
 
-
+const { Link, NavLink } = ReactRouterDOM
 export function Home() {
     return (
         <section className="home">
-            <h1>Welcome to the Notes & Miss book & Email Application!</h1>
-            <p>
-                {/* This application allows you to manage your notes, send and receive emails, and keep track of important appointments or tasks in your missbook. */}
+            <h1>Welcome to the Notes & Missbook & Email Application!</h1>
+            <p className="intro-text">
+                Manage your notes, emails, and appointments easily!
             </p>
-            {/* <h2>Features:</h2> */}
-            <ul>
-                <li><strong>Notes:</strong> Easily create, edit, and delete notes to keep track of your thoughts and tasks.</li>
-                <li><strong>Email:</strong> Send and receive emails right from this application, making communication easier.</li>
-                <li><strong>Missbook:</strong> Organize your appointments and tasks to ensure you never miss an important date.</li>
+            <div className="icon-container">
+                <NavLink onClick={() => setIsClicked(false)} to="/book">
+                    <img src="assets/img/books.png" className="img-logo" alt="Book" />
+                </NavLink>
+                <NavLink onClick={() => setIsClicked(false)} to="/mail">
+                    <img src="assets/img/email.png" className="img-logo" alt="Mail" />
+                </NavLink>
+                <NavLink onClick={() => setIsClicked(false)} to="/note">
+                    <img src="assets/img/note.png" className="img-logo" alt="Note" />
+                </NavLink>
+            </div>
+            <ul className="features-list">
+                <li><strong>Notes:</strong> Create, edit, and delete notes to stay organized.</li>
+                <li><strong>Email:</strong> Send and receive emails from within the app.</li>
+                <li><strong>Missbook:</strong> Organize appointments and tasks effortlessly.</li>
             </ul>
-            <p>
-                {/* Choose one of the features to get started! */}
+            <p className="start-text">
+                Select a feature to get started!
             </p>
         </section>
     )
